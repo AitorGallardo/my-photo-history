b7895717d9139fafbdef347da10770dc6b827c4a
Aitor Gallardo <38296940+AitorGallardo@users.noreply.github.com>
2023-08-21 01:06:16 +0200

    workaround with tailwind and anuglarmat conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
+  // corePlugins: {
+  //   preflight: false,
+  // },
+  // prefix: 'tw-',
   content: [
     "./src/**/*.{html,ts}",
     './projects/**/*.{html,ts}',
